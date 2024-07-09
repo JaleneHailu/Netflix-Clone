6ac3ae9492556bc79847c085712963843f5244e8
JaleneHailu <147465004+JaleneHailu@users.noreply.github.com>
2024-07-09 18:58:02 +0300

    move to main foldergh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: 'https://JaleneHailu.github.io/Netflix-Clone/',
+  plugins: [react()]
 })
